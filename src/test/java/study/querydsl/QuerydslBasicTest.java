d8c1f254a8a4e0b271f2ec301c51b7033994f78a
aispark <aispark@paran.com>
2021-08-07 00:47:10 +0900

    검색조건쿼리

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -66,6 +66,20 @@ public class QuerydslBasicTest {
                 .fetchOne();
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
-    }  
+    }
+
+    @Test
+    public void search() throws Exception {
+        Member findMember = jpaQueryFactory
+                .select(member)
+                .from(member)
+                .where(
+                        member.username.eq("member1"),
+                        member.age.eq(10)
+                )
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
 
 }

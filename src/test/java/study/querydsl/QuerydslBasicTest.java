f9359101b81edb9bfcd07f434bdb1a0d04002fe2
aispark <aispark@paran.com>
2021-08-07 17:23:22 +0900

    프로젝션과 결과 반환 - @QueryProjection

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberDto;
+import study.querydsl.dto.QMemberDto;
 import study.querydsl.dto.UserDto;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
@@ -644,6 +645,20 @@ public class QuerydslBasicTest {
         //then
         for (UserDto userDto : result) {
             System.out.println("userDto = " + userDto);
+        }
+    }
+    
+    @Test
+    public void findDtoByQueryProjection() throws Exception {
+        //when
+        List<MemberDto> result = queryFactory
+                .select(new QMemberDto(member.username, member.age))
+                .from(member)
+                .fetch();
+
+        //then
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
         }
     }
 }

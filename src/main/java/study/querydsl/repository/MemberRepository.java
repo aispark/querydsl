7852b292e782fccc662c90c6a97f9ed86e307665
aispark <aispark@paran.com>
2021-08-10 00:02:50 +0900

    사용자 정의 repository

--- src/main/java/study/querydsl/repository/MemberRepository.java
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -1,10 +1,11 @@
 package study.querydsl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import study.querydsl.entity.Member;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    List<Member> findByUsername(String username);
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+    List<Member> findByUsername(@Param("username") String username);
 }
